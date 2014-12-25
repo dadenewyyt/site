6ba83904676884bc78e6db24606df8ed0a9d17a0
daniel adenew <dadenewyyt@gmail.com>
2014-12-25 18:41:24 +0300

    page title for signup

--- application/views/layout/signup.php
+++ application/views/layout/signup.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Scaffolding · Bootstrap</title>
+    <title>Welcome To MadeByUs4U.com | SignUp</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
