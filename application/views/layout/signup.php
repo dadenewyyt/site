4654699cb8ec6f0ec2492bdbc46238bdc1f56697
daniel adenew <dadenewyyt@gmail.com>
2014-12-25 09:37:52 +0300

    signup view page

---
+++ application/views/layout/signup.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Scaffolding · Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href=<?php echo base_url()."assets/plugins/bootstrap/css/bootstrap.min.css";?> rel="stylesheet">
+     <link href=<?php echo base_url()."assets/css/home.css";?> rel="stylesheet">
+
+</head>
+
+<body>
+
+<header>
+
+<div class='row row1'>
+    <div class="container">
+    <div class="col-md-2 col-left-border">
+       <b>Hi</b> <a href="#">Sign In</a> or <a href="#">Register</a>
+    </div>
+     <div class="col-md-1 col-left-border">
+       <strong><a href="#">Sell</a></strong>
+    </div>
+     <div class="col-md-1 col-left-border">
+        <strong><a href="#">Buy</a></strong>
+    </div>
+     <div class="col-md-1 col-left-border">
+         <strong><a href="#">Bid</a></strong>
+    </div>
+     <div class="col-md-3 col-md-offset-3 search-box">
+         <div class="input-group">
+          <input type="text" class="form-control input-sm" style="height:28px;" placeholder = "search . . .">
+          <span class="input-group-btn">
+            <button class="btn btn-default btn-xs" type="button">search</button>
+          </span>
+        </div><!-- /input-group -->
+    
+    </div>
+     <div class="col-md-1 col-left-border col-md-11">
+      <span class=" glyphicon glyphicon-bell" aria-hidden="true"></span>
+      </div>
+      <div class="col-md-1 col-left-border col-md-11 ">
+         <span class=" glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+
+       </div>
+    
+        </div>
+    </div>
+</div>
+</header>
+
+<!-- Responsive design
+================================================== -->
+<section id="responsive" >
+<div class="container container2">
+
+<div class="row row2"  >
+ <div class="col-md-3 col-md-offset-5" style="background-color:balck;"><img src="../assets/images/logo_home.jpg"/></div>
+ <div class="col-md-2 col-md-offset-1 col-md-offset-1-1" style="margin-top: 1%;">
+          <select class="form-control" size="width:100%;">
+          <option>All Catagories</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          </select>
+  </div>
+  <div class="col-sm-1" style="margin-top: 1%;">  <input type='button' class="btn btn-primary form-control" value="search"> </div>
+
+  </div> 
+
+  <div class="row">
+
+          <nav class="navbar navbar-default" role="navigation" style="min-height:40px;">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#madebyus4u-mobile-responsive-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+                <a class="navbar-brand" href="#">
+                <img alt="Brand" src="../assets/images/logo.jpg" height="0">
+              </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="madebyus4u-mobile-responsive-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li><a href="#">Closing </a></li>
+                <li><a href="#">Footwear</a></li>
+                <li><a href="#">Outdoors</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Entertainment</a></li>
+                <li><a href="#">Technology</a></li>
+                <li><a href="#">Arts & Crafts</a></li>
+                <li><a href="#">Music</a></li>
+                <li><a href="#">Home Goods</a></li>     
+                <li><a href="#">Accessories</a></li>     
+                
+              </ul>
+             
+              
+              
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+         <div class="container container4" id='header_image'>
+                  
+            <div class=" row row3-background">
+                      <!-- /.this the large header image -->
+                  </div>
+              </div>
+
+         <div class="container">
+                          <div class="row row4">
+
+                            <h3> Welcome To MadeByUs4U! The best way to shop/bid online and communicate seller-to-buyer.</h3>
+                            
+                             <hr>
+                          </div>
+                </div>
+  </div>
+
+    <div class="row row5">
+
+         <div class="col-md-4 module_images_upload col-sm-4 col-xs-4 module_border_style">
+
+                <h4>Upload your product</h4>
+                <p>You upload your product we do the rest while you make 90% of the sale price</p>
+                <button class="btn btn-default" >EARN CASH</button>
+          
+     
+         </div>
+
+          <div class="col-md-4 col-md-offset-1 col-sm-4 col-xs-4 module_images_newarrival img-responsive module_border_style">
+             
+               <button class="btn btn-primary">NEW ARRIVALS</button>
+         
+          </div>
+
+         <div class="col-md-4 col-sm-4 col-xs-4 col-md-offset-1 module_images_meet_people img-responsive module_border_style" > 
+
+                <h4>MEET NEW MEMBERS DAILY</h4>
+
+         </div>
+    
+     </div>
+
+    <div class="row row6">
+
+            <div class="col-md-6 col-md-offset-3" >
+            <div class="content-bottom">
+                 <h2> Our Latest Arrivals </h2>
+               <p style="color:#8B8A8A"> Check our latest offers that just arrived to the store. <span>New Nonummy</span> for you to wear</p>  
+
+            </div>
+           
+            </div> 
+    </div>
+
+<div class="row row7">
+
+    <div class="row">
+        <div class='pull-right'>
+            <span class='glyphicon glyphicon-chevron-left' style='background-color:#cccccc;'></span>
+            <span class='glyphicon glyphicon-chevron-right' style='background-color:#cccccc;'></span>
+            </div>
+     </div>
+
+<div class='row'>
+
+  <div class="col-sm-3 col-md-3">
+        <div class="thumbnail">
+        
+        <!-- <img data-src="holder.js/200x300">
+         <img src="../assets/images/products/ring.jpg" width="200" height="300">
+         -->
+            <img data-src="holder.js/100%x200" alt="100%x200"  class="img-thumbnail img-responsive" src="../assets/images/products/dimond.jpg" data-holder-rendered="true" style="height: 200px; display: block;">
+     
+                  <div class="caption">
+                        <h4>Set of Wedding Ring</h4>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p><span class='price'>$59.89</span><span class='buy_btn'><a href="#" class="btn btn-primary" role="button">Buy Now!</a></span></p>
+                  </div>
+                  <hr>
+                  <p><span class='seller_name'><i class='glyphicon glyphicon-user'><b style='padding-left:4px;color: #1f72ad;'>Seller Name</b></span></i><span class='rating_class'><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star-empty'></i><i class='glyphicon glyphicon-star-empty'></i></span></span></p>
+
+
+        </div>
+    </div>
+
+    <div class="col-sm-3 col-md-3">
+            <div class="thumbnail">
+                <img data-src="holder.js/100%x200" alt="100%x200"  class="img-thumbnail img-responsive" src="../assets/images/products/tshirt.jpg" data-holder-rendered="true" style="height: 200px;display: block;">
+                  <div class="caption">
+                    <h4>Black and Red Plad Sleeve</h4>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><span class='price'>$59.89</span><span class='buy_btn'><a href="#" class="btn btn-primary" role="button">Buy Now!</a></span></p>
+                  </div>
+                   <hr>
+                  <p><span class='seller_name'><i class='glyphicon glyphicon-user'><b style='padding-left:4px;color: #1f72ad;'>Seller Name</b></span></i><span class='rating_class'><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star-empty'></i><i class='glyphicon glyphicon-star-empty'></i></span></span></p>
+            </div>
+      </div>
+
+    <div class="col-sm-3 col-md-3">
+        <div class="thumbnail">
+        <img data-src="holder.js/100%x200" alt="100%x200"  class="img-thumbnail img-responsive" src="../assets/images/products/bag.jpg" data-holder-rendered="true" style="height: 200px;display: block;">
+          <div class="caption">
+            <h4>Black and Red Plad Sleeve</h4>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><span class='price'>$59.89</span><span class='buy_btn'><a href="#" class="btn btn-primary" role="button">Buy Now!</a></span></p>
+          </div>
+           <hr>
+          <p><span class='seller_name'><i class='glyphicon glyphicon-user'><b style='padding-left:4px;color: #1f72ad;'>Seller Name</b></span></i><span class='rating_class'><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star-empty'></i><i class='glyphicon glyphicon-star-empty'></i></span></span></p>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-md-3">
+
+        <div class="thumbnail">
+           <img data-src="holder.js/100%x200" alt="100%x200"  class="img-thumbnail img-responsive" src="../assets/images/products/ring.jpg" data-holder-rendered="true" style="height: 200px;display: block;">
+           <div class="caption">
+           <h4>Set of Wedding Ring</h4>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><span class='price'>$59.89</span><span class='buy_btn'><a href="#" class="btn btn-primary" role="button">Buy Now!</a></span></p>
+          </div>
+           <hr>
+                  <p><span class='seller_name'><i class='glyphicon glyphicon-user'><b style='padding-left:4px;color: #1f72ad;'>Seller Name</b></span></i><span class='rating_class'><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star-empty'></i><i class='glyphicon glyphicon-star-empty'></i></span></span></p>
+         
+        </div>
+      </div>
+
+    </div>
+
+  
+  </div>
+  <div class="row">
+
+ <div class="col-sm-3 col-md-3">
+        <div class="thumbnail">
+        
+        <!-- <img data-src="holder.js/200x300">
+         <img src="../assets/images/products/ring.jpg" width="200" height="300">
+         -->
+            <img data-src="holder.js/100%x200" alt="100%x200"  class="img-thumbnail img-responsive" src="../assets/images/products/dimond.jpg" data-holder-rendered="true" style="height: 200px; display: block;">
+     
+                  <div class="caption">
+
+                       <h4>Set of Wedding Ring</h4>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p><span class='price'>$59.89</span><span class='buy_btn'><a href="#" class="btn btn-primary" role="button">Buy Now!</a></span></p>
+                  </div>
+                  <hr>
+                  <p><span class='seller_name'><i class='glyphicon glyphicon-user'><b style='padding-left:4px;color: #1f72ad;'>Seller Name</b></span></i><span class='rating_class'><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star-empty'></i><i class='glyphicon glyphicon-star-empty'></i></span></span></p>
+             
+                 
+        </div>
+    </div>
+
+    <div class="col-sm-3 col-md-3">
+            <div class="thumbnail">
+                <img data-src="holder.js/100%x200" alt="100%x200"  class="img-thumbnail img-responsive" src="../assets/images/products/tshirt.jpg" data-holder-rendered="true" style="height: 200px;display: block;">
+                  <div class="caption">
+                    <h4>Black and Red Plad Sleeve</h4>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                     <p><span class='price'>$59.89</span><span class='buy_btn'><a href="#" class="btn btn-primary" role="button">Buy Now!</a></span></p>
+                  </div>
+                   <hr>
+                  <p><span class='seller_name'><i class='glyphicon glyphicon-user'><b style='padding-left:4px;color: #1f72ad;'>Seller Name</b></span></i><span class='rating_class'><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star-empty'></i><i class='glyphicon glyphicon-star-empty'></i></span></span></p>
+            </div>
+      </div>
+
+    <div class="col-sm-3 col-md-3">
+        <div class="thumbnail">
+        <img data-src="holder.js/100%x200" alt="100%x200"  class="img-thumbnail img-responsive" src="../assets/images/products/bag.jpg" data-holder-rendered="true" style="height: 200px;display: block;">
+          <div class="caption">
+            <h4>Black and Red Plad Sleeve</h4>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><span class='price'>$59.89</span><span class='buy_btn'><a href="#" class="btn btn-primary" role="button">Buy Now!</a></span></p>
+          </div>
+           <hr>
+                  <p><span class='seller_name'><i class='glyphicon glyphicon-user'><b style='padding-left:4px;color: #1f72ad;'>Seller Name</b></span></i><span class='rating_class'><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star-empty'></i><i class='glyphicon glyphicon-star-empty'></i></span></span></p>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-md-3">
+
+        <div class="thumbnail">
+           <img data-src="holder.js/100%x200" alt="100%x200"  class="img-thumbnail img-responsive" src="../assets/images/products/ring.jpg" data-holder-rendered="true" style="height: 200px;display: block;">
+           <div class="caption">
+            <h4>Black and Red Plad Sleeve</h4>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+             <p><span class='price'>$59.89</span><span class='buy_btn'><a href="#" class="btn btn-primary" role="button">Buy Now!</a></span></p>
+          </div>
+           <hr>
+                  <p><span class='seller_name'><i class='glyphicon glyphicon-user'><b style='padding-left:4px;color: #1f72ad;'>Danny man</b></span></i><span class='rating_class'><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star'></i><i class='glyphicon glyphicon-star-empty'></i><i class='glyphicon glyphicon-star-empty'></i></span></span></p>
+           
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+  
+  
+
+  
+  
+
+  
+             
+</div>  
+
+
+
+</section>
+<footer class="footer">
+<?php $this->load->view($footer_page); ?>
+</footer>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src=<?php base_url()."assets/js/holder.js";?>></script>
+<script src="path/to/js/star-rating.min.js" type="text/javascript"></script>
+
+
+
+
+
+</body>
+</html>
