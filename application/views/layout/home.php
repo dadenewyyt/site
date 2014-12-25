b9953d1b836829e218f540dfe88c362a3ca45e5e
daniel adenew <dadenewyyt@gmail.com>
2014-12-25 18:48:19 +0300

    page title home fix

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Scaffolding · Bootstrap</title>
+    <title>Welcome to MadeByUs4U.con | Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
