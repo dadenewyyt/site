6e4707b9d2ea5694e9b6b7a48d64f1e02e8acd3f
daniel adenew <dadenewyyt@gmail.com>
2015-01-01 16:49:32 +0300

    merged with master

---
+++ application/views/include/header_black_menu.php
@@ -0,0 +1,60 @@
+ <div class='row row1'>
+        <div class="container">
+            <div class="col-md-2 col-left-border">
+                <b>Hi</b> <a href="#">Sign In</a> or <a href="<?php echo base_url('signup');?>">Register</a>
+            </div>
+            <div class="col-md-1 col-left-border">
+                <strong><a href="#">Sell</a></strong>
+            </div>
+            <div class="col-md-1 col-left-border">
+                <strong><a href="#">Buy</a></strong>
+            </div>
+            <div class="col-md-1 col-left-border">
+                <strong><a href="<?php echo base_url('bid');?>">Bid</a></strong>
+            </div>
+            <div class="col-md-2 col-md-offset-3 col-left-border" style="text-align:right;">
+                My Account
+
+            </div>
+
+            <div class="col-md-1 col-left-border ">
+                Checkout
+            </div>
+            <div class="col-md-1 col-left-border">
+                <a href="#">Login</a>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="row row2 paddings">
+
+        <div class="container">
+            <div class="col-md-2" >
+                <div class='shopping_cart'>
+                    <small style='padding-right:5px;font-size:1.3em;color:grey;'>Cart/ $ 0.00</small>
+                    <span class="glyphicon glyphicon-shopping-cart" ></span>
+                    <sup class="badge">0</sup>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-md-offset-3 ">
+                <a href="<?php echo base_url('welcome/home'); ?>"><img src="<?php echo base_url()."assets/images/logo_home.jpg";?>"/></a>
+            </div>
+
+            <div class="col-md-2 col-md-offset-1 col-md-offset-1-1" style="margin-top: 1%;">
+                <select class="form-control" size="width:100%;">
+                    <option>All Catagories</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            <div class="col-sm-1" style="margin-top: 1%;">  <input type='button' class="btn btn-primary form-control" value="search"> </div>
+
+        </div>
+
+    </div>
---
+++ application/views/include/tweets.php
@@ -0,0 +1,36 @@
+ <style type="text/css">
+
+.header_text {
+
+font-weight: 600;
+font-size: 16px;
+}
+.tweet_content {
+
+background-image: url('../../assets/images/bid/twitter_large.png');
+background-repeat: none;
+height: 50;
+border-bottom: 2px dotted grey;
+padding: 7px;
+}
+
+ </style>
+
+  <div class="image">
+  	<P>
+  	<img src="<?php echo base_url()."assets/images/bid/twitter_small.png";?>" height=20 >
+     <span class="header_text">Latest From Twitter</span>
+  	</P>
+  </div>
+  
+  <div class="col-md-11">
+
+    <div class="twitte_text" style="width:100%;">
+		<?php 
+         foreach($tweetes_content as $twitted_text)  {          
+          echo "<p class='tweet_content'>".$twitted_text."</p>";
+        }
+
+		?>
+            </div>
+  </div>
