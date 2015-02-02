d887ea0a0a70ba5dabb5d0e6d188cbc0a2353f5c
daniel adenew <dadenewyyt@gmail.com>
2015-02-02 18:58:45 +0300

    adding links

--- application/views/include/header_black_menu.php
+++ application/views/include/header_black_menu.php
@@ -1,74 +1,3 @@
-<!-- <style type="text/css">
-.col-md-1 a ,.col-md-2 a {
-    font-weight: 800;
-    font-size: 12px;
-}
-
-header {
-background-color: #ffffff;
-}
-
-.navbar {
-    min-height:40px;
-    border-radius:0px;
-    width:100%;   
-}
-
-.navbar-default .navbar-nav>li>a {
-color: #000000;
-padding-right: 15px;
-}
-
-.navbar-default .navbar-nav>li>a:hover, .navbar-default .navbar-nav>li>a:focus {
-
-color: #a1a1a1;
-
-}
-.navbar-nav {
-  width: 100%;
-  margin-left: -16px;
-}
-.navbar {
-
-}
-
-</style>
- </style>
- <div class='row row1'>
-        <div class="container">
-            <div class="col-md-2 col-left-border">
-                <b>Hi</b> <a href="<?php echo base_url('users/login');?>">Sign In</a> or <a href="<?php echo base_url('signup');?>">Register</a>
-            </div>
-            <div class="col-md-1 col-left-border">
-                <strong><a href="<?php echo base_url('buy');?>">Shop</a></strong>
-            </div>
-            <div class="col-md-1 col-left-border">
-                <strong><a href="<?php echo base_url('sell');?>">Sell</a></strong>
-            </div>
-            <div class="col-md-1 col-left-border">
-                <strong><a href="<?php echo base_url('bid');?>">Bid</a></strong>
-            </div>
-            <div class="col-md-2 col-md-offset-3 col-left-border" style="text-align:right;">
-                My Account
-            </div>
-
-            <div class="col-md-1 col-left-border ">
-                Checkout
-            </div>
-            <div class="col-md-1 col-left-border">
-             <?php $logged_in = (bool) $this->session->userdata('logged_in'); ?>
-             <?php if($logged_in===TRUE): ?>
-             <a href="<?php echo base_url('users/logout');?>">Logout</a>
-              <?php else: ?>
-              <a href="<?php echo base_url('users/login');?>">Login</a>
-              <?php  endif ?>
-
-            </div>
-
-        </div>
-
-    </div>
--->
 <div class="row">
 <style type="text/css">
 .navbar-topmenu.navbar-inverse .navbar-text {
@@ -168,9 +97,9 @@ overflow: hidden;
                    </p>
             <?php  endif ?>
         <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Shop</a></li>
-        <li><a href="#">Sell</a></li>
-        <li><a href="#">Bid</a></li>
+        <li><a href="<?php echo base_url('buy/buy');?>">Shop</a></li>
+        <li><a href="<?php echo base_url('sell/sell');?>">Sell</a></li>
+        <li><a href="<?php echo base_url('bid/bid');?>">Bid</a></li>
        
       </ul>
      
